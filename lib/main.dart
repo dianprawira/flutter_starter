07ecd026a9ed74c0402e4fff9f4f3481c122e0be
dianprawira <dianprawira@untan.ac.id>
2022-10-24 06:14:33 +0700

    Navigation complete sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_starter/ui/another_screen.dart';
+import 'package:flutter_starter/ui/replacement_screen.dart';
+import 'package:flutter_starter/ui/return_data_screen.dart';
+import 'package:flutter_starter/ui/second_screen.dart';
+import 'package:flutter_starter/ui/second_screen_with_data.dart';
 import 'ui/my_home_page.dart';
 
 void main() {
@@ -15,9 +20,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(
-        title: 'Selamat Datang',
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(
+              title: 'Depan',
+            ),
+        '/secondScreen': (context) => const SecondScreen(),
+        '/secondScreenWithData': (context) => SecondScreenWithData(
+            data: ModalRoute.of(context)?.settings.arguments as String),
+        '/returnDataScreen': (context) => const ReturnDataScreen(),
+        '/replacementScreen': (context) => const ReplacementScreen(),
+        '/anotherScreen': (context) => const AnotherScreen(),
+      },
     );
   }
 }
--- lib/ui/my_home_page.dart
+++ lib/ui/my_home_page.dart
@@ -4,14 +4,49 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: const Center(
-        child: Text('Halo Semuanya'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: const Text('Go to Second Screen'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/secondScreen');
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Navigation with Data'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/secondScreenWithData',
+                    arguments: 'Hello from First Screen!');
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Return Data from Another Screen'),
+              onPressed: () async {
+                final scaffoldMessenger = ScaffoldMessenger.of(context);
+                final result =
+                    await Navigator.pushNamed(context, '/returnDataScreen');
+                SnackBar snackBar = SnackBar(content: Text('$result'));
+                scaffoldMessenger.showSnackBar(snackBar);
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Replace Screen'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/replacementScreen');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
